8332d9e9532f2014f2dc59cf7e77d47f6c2daf52
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-12-26 14:00:09 -0800

    login create setup on controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,4 +1,15 @@
 class Users::SessionsController < Devise::SessionsController
-    respond_to :json
-    # additional custom methods
+  respond_to :json
+
+  # This is Devise's method for signing in a user
+  # Override it to customize the sign-in process
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    if resource
+      sign_in(resource_name, resource)
+      render json: { message: 'Login successful', user: resource }, status: :ok
+    else
+      render json: { error: 'Invalid credentials' }, status: :unauthorized
+    end
   end
+end
