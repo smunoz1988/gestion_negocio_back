6a84c349171490f7c73f71ac0ec5b7bd98dc9520
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-12-30 17:37:21 -0800

    linter fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::API
-    before_action :configured_permitted_parameters, if: :devise_controller?
+  before_action :configured_permitted_parameters, if: :devise_controller?
 
-    protected
+  protected
 
-    def configured_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :name, :last_name, :document_id, :address, :phone, :role, :sede])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:name, :last_name, :document_id, :address, :phone, :role])
-    end
+  def configured_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: %i[email password password_confirmation name last_name document_id address
+                                               phone role sede])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name last_name document_id address phone role])
+  end
 end
