42c43209a21f25c9862d5193b70c1ce181c23a17
Daniela Moreno <dani9292@hotmail.com>
2024-01-02 12:13:51 -0500

    create routes for professionals in api v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   get '/current_user', to: 'current_user#index'
+  namespace :api do
+    namespace :v1 do
+      resources :professionals, only: [:index, :show, :create, :update, :destroy]
+    end
+  end
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
