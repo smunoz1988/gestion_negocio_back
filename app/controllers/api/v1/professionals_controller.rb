ace3e7cf4a3f4e439055ff26b5c1030fb864554e
Daniela Moreno <dani9292@hotmail.com>
2024-01-02 12:30:26 -0500

    create api for professionals

--- app/controllers/api/v1/professionals_controller.rb
+++ app/controllers/api/v1/professionals_controller.rb
@@ -1,2 +1,41 @@
 class Api::V1::ProfessionalsController < ApplicationController
+
+  # create professionals
+  def create
+    @professional = Professional.new(professional_params)
+    if @professional.save
+      render json: @professional, status: :created
+    else
+      render json: @professional.errors, status: :unprocessable_entity
+    end
+  end
+
+  # get ALL professionals
+  def index
+    professionals = Professional.all
+    render json: professionals, status: :ok
+  end
+
+  # get ONE professional
+  def show
+    professional = Professional.find(params[:id])
+    render json: professional, status: :ok
+  end
+
+  # update ONE professional
+  def update
+    professional = Professional.find(params[:id])
+    if professional.update(professional_params)
+      render json: @professional, status: :ok
+    else
+      render json: @professional.errors, status: :unprocessable_entity
+    end
+  end
+
+  # delete ONE professional
+  def destroy
+    professional = Professional.find(params[:id])
+    professional.destroy
+    render json: professional, status: :ok
+  end
 end
